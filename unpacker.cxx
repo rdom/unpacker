8672ce05e0561d3893d2bcc26c022b23ebec18b0
daq4 <daq4@d.d>
2018-07-28 13:55:30 +0200

    change input file regex

--- unpacker.cxx
+++ unpacker.cxx
@@ -27,7 +27,7 @@ int main(int argc, const char ** argv){
 
   string tdcAddresses("tdc.list");
   string hubAddresses("hub.list");
-  string inRegex("/d/jul2018/beam*.hld");
+  string inRegex("/d/jul2018/*.hld");
   //string inRegex("../data/beam*.hld");
   string inFile("../data/dd15110143027.hld");
   
